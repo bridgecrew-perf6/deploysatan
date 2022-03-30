1d03583bc1d7cefc343dd4f1522ea55b0190655d
multipledisclosures <44260675+multipledisclosures@users.noreply.github.com>
2022-03-30 14:07:39 +0800

    no

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       This is multipledisclosures.github.io/deploysatan
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is multipledisclosures.github.io/deploysatan
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
