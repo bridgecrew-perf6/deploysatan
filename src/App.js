7a370235c0d533532cd5a73d2c269703586da146
multipledisclosures <44260675+multipledisclosures@users.noreply.github.com>
2022-03-30 14:05:18 +0800

    lol

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
        This is multipledisclosures.github.io/deploysatan
-       </p>
-      </header>
     </div>
   );
 }
